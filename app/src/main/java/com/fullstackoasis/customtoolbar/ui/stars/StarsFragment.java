5c922509c9b73f5ba061c0c18cf128b419bcc3af
fullStackOasis <fullstackdev@fullstackoasis.com>
2021-07-29 20:15:32 -0400

    Add new Stars Fragment, similar to MainFragment.

---
+++ app/src/main/java/com/fullstackoasis/customtoolbar/ui/stars/StarsFragment.java
@@ -0,0 +1,39 @@
+package com.fullstackoasis.customtoolbar.ui.stars;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fullstackoasis.customtoolbar.R;
+import com.fullstackoasis.customtoolbar.ui.main.MainFragment;
+import com.fullstackoasis.customtoolbar.ui.main.MainViewModel;
+
+public class StarsFragment extends Fragment {
+
+    private StarsViewModel mViewModel;
+
+    public static StarsFragment newInstance() {
+        return new StarsFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.stars_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(StarsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/fullstackoasis/customtoolbar/ui/stars/StarsViewModel.java
@@ -0,0 +1,7 @@
+package com.fullstackoasis.customtoolbar.ui.stars;
+
+import androidx.lifecycle.ViewModel;
+
+public class StarsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
