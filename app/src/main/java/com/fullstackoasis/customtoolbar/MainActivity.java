d3c8567f42f7089a42d1099456a280f1670bc8ce
fullStackOasis <fullstackdev@fullstackoasis.com>
2021-07-29 20:20:48 -0400

    Make stars button interactive. It will navigate to StarsFragment.

--- app/src/main/java/com/fullstackoasis/customtoolbar/MainActivity.java
+++ app/src/main/java/com/fullstackoasis/customtoolbar/MainActivity.java
@@ -3,19 +3,34 @@ package com.fullstackoasis.customtoolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.fullstackoasis.customtoolbar.ui.main.MainFragment;
+import com.fullstackoasis.customtoolbar.ui.stars.StarsFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton btnStars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+        btnStars = findViewById(R.id.imageButtonStars);
+        btnStars.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, StarsFragment.newInstance())
+                        .commitNow();
+
+            }
+        });
     }
 }
